89cd5dcc78e9af493d2cb8109f791c00481cd2e9
ahmedraza55592 <ahmedraza55592@gmail.com>
2021-02-27 17:29:37 +0500

    Day 2 Completed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,15 +1,22 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class HomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Catalog App"),
-//       ),
-//       body: Container(
-//         child: Center(child: Text("Welcome")),
-//       ),
-//     );      
-//   }
-// }
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int number = 12;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Catalog"),
+      ),
+      body: Material(
+        child: Container(
+          child: Center(
+            child: Text("Welcome $number to flutter"),
+          ),
+        ),
+      ),
+      drawer: Drawer(child: Center(child: Text("First")),),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/homepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,14 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Container(
-          child: Center(
-            child: Text(
-              "Welcome to flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
